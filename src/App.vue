ac8085f0edf4822e603b521b476252b7e1999f1f
hi94740 <hi94740@qq.com>
2020-08-29 03:54:05 +0800

    打完鲑鱼不知道为什么就是很想把这个做完

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>bilibili追番日历</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{on}">
+                    <v-btn href="" icon large target="_blank" v-on="on">
+                      <v-icon>mdi-code-tags</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>以后还会增加更多功能！</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    v-model="buid"
+                    label="输入b站uid"
+                    name="buid"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    :rules="[validateBUID]"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  :disabled="!isBUIDvalid"
+                  outlined
+                  v-clipboard:copy="
+                    'https://calendars.hi94740.workers.dev/bilibili/bangumi.ics?uid=' +
+                      buid
+                  "
+                  >拷贝ics链接</v-btn
+                >
+                <v-btn
+                  color="primary"
+                  :disabled="!isBUIDvalid"
+                  :href="
+                    'webcal://calendars.hi94740.workers.dev/bilibili/bangumi.ics?uid=' +
+                      buid
+                  "
+                  >订阅日历</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: "App",
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    data: () => ({
+      buid: ""
+    }),
+
+    computed: {
+      isBUIDvalid() {
+        return /^[1-9]\d*$/.test(this.buid) && parseInt(this.buid) > 0
+      }
+    },
+
+    methods: {
+      validateBUID() {
+        return this.isBUIDvalid || "uid必须是数字！"
+      }
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
